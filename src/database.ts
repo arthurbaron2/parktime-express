084cd8bf39b260db3a683c52c0d4f75a14392a3c
Arthur BARON <arthur.baron@adevinta.com>
2025-10-20 00:51:50 +0200

    fix dotenv db

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,9 @@
-import pg from "pg";
+import pg from 'pg'
+import dotenv from 'dotenv'
 
-const { Pool } = pg;
+const { Pool } = pg
+
+dotenv.config()
 
 export const pool = new Pool({
   host: process.env.PGHOST,
@@ -8,8 +11,8 @@ export const pool = new Pool({
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
   port: Number(process.env.PGPORT) || 5432,
-});
+})
 
-pool.on("error", (err) => {
-  console.error("❌ PostgreSQL connection error", err);
-});
+pool.on('error', (err) => {
+  console.error('❌ PostgreSQL connection error', err)
+})
